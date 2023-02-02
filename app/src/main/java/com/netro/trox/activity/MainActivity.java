4a2b0d7d516a4bd56629d384a81957dea5819c5d
Ahamed Lincon <52606514+MrLincon@users.noreply.github.com>
2023-02-02 18:18:59 +0600

    Initial commit

---
+++ app/src/main/java/com/netro/trox/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.netro.trox.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.netro.trox.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/netro/trox/activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.netro.trox.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.netro.trox.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
